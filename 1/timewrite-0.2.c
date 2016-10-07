9ed54a6f824bcf2746c93eeb983bed6d68fab0fa
csdnn <2734419815@qq.com>
2016-10-07 22:33:18 +0800

    first commit

---
+++ 1/timewrite-0.2.c
@@ -0,0 +1,60 @@
+#include "apue.h"
+#include "sys/times.h"
+#include "sys/stat.h"
+#include "fcntl.h"
+#define MAX_BUFFSIZE 131072
+#define INIT_BUFFSIZE 256
+
+int
+main(int argc, char *argv[])
+{
+    struct tms t_start,t_end;
+    long long CLOCKS_PER_SEC =  sysconf(_SC_CLK_TCK);
+    int    outfile,i;    
+    long   cur_buffsize, input_length,  n, loops;
+    char   buf[MAX_BUFFSIZE];
+    char *tmp_buf = NULL;//temp buff
+    
+    if(argv[2] == "SYNC")     outfile = open(argv[1], O_RDWR|O_CREAT|O_SYNC, S_IRUSR|S_IWUSR);
+       else                   outfile = open(argv[1], O_RDWR|O_CREAT, S_IRUSR|S_IWUSR);
+     
+    input_length = lseek(STDIN_FILENO, 0, SEEK_END);
+    
+    tmp_buf = (char*)malloc(input_length);
+    
+    lseek(STDIN_FILENO, 0, SEEK_SET);
+    if(read(STDIN_FILENO, tmp_buf, input_length) != input_length) 
+                 err_sys("read error~~");//can be sure it is read into tmp_buf
+    
+
+    printf("BUFFSIZE\tUSER CPU\tSYSTEM CPU\tCLOCK TIME\tLOOPS\n");
+    
+
+    for(cur_buffsize = INIT_BUFFSIZE; cur_buffsize <= MAX_BUFFSIZE; cur_buffsize *= 2)
+    {
+    	clock_t start = times(&t_start);
+        loops = 0;
+
+        n = input_length/cur_buffsize;
+        
+        for(i=0;i<n;i++){
+            if(write(outfile, tmp_buf+i*cur_buffsize, cur_buffsize)==cur_buffsize) 
+               lseek(outfile, 0, SEEK_SET);
+               loops++;
+        }
+        if(write(outfile, tmp_buf+n*cur_buffsize, input_length%cur_buffsize)>0)
+             loops++;
+
+        printf("n= %d\n",n);
+    	if (n < 0)
+        	err_sys("read error!!!");
+    
+    	clock_t end = times(&t_end);
+   	printf("%ld\t\t",cur_buffsize);
+    	printf("%.2lf\t\t",(double)(t_end.tms_utime - t_start.tms_utime)/CLOCKS_PER_SEC ); 
+    	printf("%.2lf\t\t",(double)(t_end.tms_stime - t_start.tms_stime)/CLOCKS_PER_SEC );
+    	printf("%.2lf\t\t",(double)(end - start)/CLOCKS_PER_SEC );
+    	printf("%ld\n",loops);
+    }
+    exit(0);
+}
